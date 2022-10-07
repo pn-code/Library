c746c9ce17da829a395eb373c55db87b5c3f45ea
Philip Nguyen <philipnguyen953@gmail.com>
2022-10-07 09:59:34 -0700

    refactor code with factory functions and array map

--- script.js
+++ script.js
@@ -2,92 +2,104 @@
 
 const myLibrary = []
 
-// HTML to JS links
+// HTML to JS Selectors
 
-const bookDisplay = document.querySelector('#bookDisplay')
-const addBookButton = document.querySelector('#addBookButton')
+const bookDisplay = document.getElementById('bookDisplay')
+const bookForm = document.getElementById('book-form')
 
-//input fields
-const bookTitleInput = document.querySelector('#bookTitleInput')
-const authorInput = document.querySelector('#authorInput')
-const pageNumbers = document.querySelector('#pageNumbers')
-const readStatus = document.querySelector('#readStatus')
+// input fields
+
+const bookTitleInput = document.getElementById('bookTitleInput')
+const authorInput = document.getElementById('authorInput')
+const pageNumbers = document.getElementById('pageNumbers')
+const readStatus = document.getElementById('readStatus')
 
 //Book Constructor
 
-function Book(title, author, pages, read) {
-    this.title = title, 
-    this.author = author,
-    this.pages = pages
-    this.read = read
-    this.input = function () { 
-        return this.title + " by " + this.author + ", has " + this.pages + " pages. " 
-        + "READ STATUS: " +this.read
+// function Book(title, author, pages, read) {
+//     this.title = title, 
+//     this.author = author,
+//     this.pages = pages
+//     this.read = read
+//     this.input = function () { 
+//         return this.title + " by " + this.author + ", has " + this.pages + " pages. " 
+//         + "READ STATUS: " +this.read
+//     }
+// }
+
+// Book Factory Function
+const BookFactory = (title, author, pageNumber, readStatus) => {
+    // returns an object with the information passed through as its properties
+    return {
+        title: title,
+        author: author,
+        pageNumber: pageNumber,
+        readStatus: readStatus,
+        info() {
+            return `${this.title} by ${this.author}, has ${this.pageNumber} pages; it is ${this.readStatus}.`
+        }
     }
-}
-
-//Clear Button
-const clearAllButton = document.querySelector('#clearButton')
-clearAllButton.addEventListener('click', () => {
-    myLibrary.splice(0, Infinity)
-    bookDisplay.innerText = ''
-    }
-)
-
-//Updates Display w/ DOM Manipulation
-function displayBooks(){
-    bookDisplay.innerHTML = '' //clears array from screen, so it does not repeat
-
-    for (let i = 0; i < myLibrary.length; i++){
-
-        //Display Books from myLibrary array
-        const bookDisplayContents = document.createElement('div')
-        bookDisplayContents.innerText = myLibrary[i].input() // set div to book input
-        bookDisplay.appendChild(bookDisplayContents)
-
-        //Adds remove buttons next to each book
-        const bookRemoveButton = document.createElement('button')
-        bookRemoveButton.innerText = "Remove"
-        bookDisplayContents.appendChild(bookRemoveButton)
-
-        //Adds change read status buttons next to each book
-        const bookChangeStatusButton = document.createElement('button')
-        bookChangeStatusButton.innerText = "Change Read Status"
-        bookDisplayContents.appendChild(bookChangeStatusButton)
-
-        //Remove Button Functionality
-        bookRemoveButton.addEventListener('click', () => {
-            myLibrary.splice(i, 1)
-            bookDisplayContents.innerText = ''
-            }
-        )
-        
-        //Read Status Change Button Functionality
-        bookChangeStatusButton.addEventListener('click', () => {
-            if (myLibrary[i].read == "unread"){
-                myLibrary[i].read = "read"
-                bookDisplayContents.innerText = ''
-                displayBooks()
-            }else{
-                myLibrary[i].read = "unread"
-                bookDisplayContents.innerText = ''
-                displayBooks()
-
-            }
-        })
-
-    }
-
 }
 
 //Add Book Button
-addBookButton.addEventListener('click', () => {
-    const book = new Book(bookTitleInput.value, authorInput.value, pageNumbers.value, readStatus.value);
+bookForm.addEventListener('submit', (e) => {
+    // prevents form submit to refresh website
+    e.preventDefault();
+
+    // Creates a book object depending on the inputs
+    const book = BookFactory(bookTitleInput.value, authorInput.value, pageNumbers.value, readStatus.value);
+
+    // pushes book object in to myLibrary array
     myLibrary.push(book);
-    displayBooks();
+
+    // renders book using DOM manipulation
+    renderBooks()
+
+    // clears previous book inputs
     bookTitleInput.value = '';
     authorInput.value = '';
     pageNumbers.value = '';
     readStatus.value = '';
-})
+
+});
+
+//Renders books from array on to display w/ DOM Manipulation
+const renderBooks = () => {
+    // clears prev. DOM elements from screen, so it does not repeat
+    bookDisplay.innerText = '' 
+
+    myLibrary.map(book => {
+        //Display Books from myLibrary array
+        const bookItem = document.createElement('div');
+        bookItem.innerText = book.info();
+        bookDisplay.appendChild(bookItem);
+
+        //Adds remove buttons next to each book
+        const removeBtn = document.createElement('button')
+        removeBtn.innerText = "X"
+        bookItem.appendChild(removeBtn)
+
+        //Adds change read status buttons next to each book
+        const changeStatusBtn = document.createElement('button')
+        changeStatusBtn.innerText = "Change Read Status"
+        bookItem.appendChild(changeStatusBtn)
+
+        //Remove Button Functionality
+        removeBtn.addEventListener('click', () => {
+            myLibrary.splice(myLibrary.indexOf(book), 1)
+            bookItem.remove();
+        });
+        
+        // Read Status Change Button Functionality
+        changeStatusBtn.addEventListener('click', () => {
+            book.readStatus === "unread" ? 
+            book.readStatus = "read" : book.readStatus = "unread";
+            renderBooks();
+        })
+    })
+};
+
+
+
+
 
